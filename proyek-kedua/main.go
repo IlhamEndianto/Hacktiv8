4b312e0f7e9e2434b88d0ffc6858f1a4e70b86f6
R. Bayu Ilham Endianto <bayu.ilhamendianto@gmail.com>
2023-01-13 21:28:57 +0700

    add second page

--- proyek-kedua/main.go
+++ proyek-kedua/main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 const Address = "localhost:8080"
 
+type ResponseMessage struct {
+	Message string `json:"message"`
+}
+
 func main() {
 
 	http.HandleFunc("/", LandingPage)
+	http.HandleFunc("/one", PrintLetterOne)
 	log.Fatal(http.ListenAndServe(Address, nil))
 }
 
 func LandingPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Write([]byte("Hello World"))
+	}
+}
+
+func PrintLetterOne(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		responseMessage := ResponseMessage{"1"}
+		responseInByte, _ := json.Marshal(responseMessage)
+		w.Write(responseInByte)
 	}
 }
