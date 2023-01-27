f3194bcac79bcb66e9af807ad008f7a09950f3c2
R. Bayu Ilham Endianto <bayu.ilhamendianto@gmail.com>
2023-01-27 20:21:28 +0700

    adjust directory import

--- session-3/clientserver/services/service-garage/main.go
+++ session-3/clientserver/services/service-garage/main.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"net"
 
-	"github.com/IlhamEndianto/Hacktiv8/session-3/clientserver/common/config"
-	"github.com/IlhamEndianto/Hacktiv8/session-3/clientserver/common/model"
+	"Hacktiv8project/session-3/clientserver/common/config"
+	"Hacktiv8project/session-3/clientserver/common/model"
+
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 )
--- session-3/clientserver/services/service-user/main.go
+++ session-3/clientserver/services/service-user/main.go
@@ -7,8 +7,9 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/IlhamEndianto/Hacktiv8/session-3/clientserver/common/config"
-	"github.com/IlhamEndianto/Hacktiv8/session-3/clientserver/common/model"
+	"Hacktiv8project/session-3/clientserver/common/config"
+	"Hacktiv8project/session-3/session-3/clientserver/common/model"
+
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
