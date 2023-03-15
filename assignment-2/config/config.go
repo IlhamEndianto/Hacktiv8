17960c405a4f1d674c9513b6e90d38325017416e
R. Bayu Ilham Endianto <bayu.ilhamendianto@gmail.com>
2023-03-15 16:35:42 +0700

    checkpoint error db migration

--- assignment-2/config/config.go
+++ assignment-2/config/config.go
@@ -3,7 +3,7 @@ package config
 const (
 	PostgresUsername = "postgresuser"
 	PostgresPassword = "postgrespassword"
-	PostgresAddress  = "postgres://postgresuser:postgrespassword@localhost:5432/postgres?sslmode=disable"
+	PostgresAddress  = "postgres://postgresuser:postgrespassword@postgres:5432/postgres?sslmode=disable"
 	WebServerPort    = ":8080"
 	SessionId        = "test-id"
 )
